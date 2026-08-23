aa7e0a6916afbd8f4624a445ba10e19d178276fa
JainAtishya <atishyajain62149@gmail.com>
2026-08-23 20:23:50 +0530

    prepare frontend for production deployment

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-
 const apiClient = axios.create({
-
-    baseURL: "/api/v1",
-
+    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
     withCredentials: true
-
 });
 
 
